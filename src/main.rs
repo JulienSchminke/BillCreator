525e912302f3e3ec5d074abf6cea4b1bb0b8b379
Julien Schminke <julien.schminke@web.de>
2023-06-17 09:32:53 +0200

    updated .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use genpdf::{Document, fonts, PaperSize, elements::{Text, TableLayout, LinearLayout, FrameCellDecorator}, Margins, SimplePageDecorator, style::{Style, StyledString}, error::Error};
+use genpdf::{Document, fonts, PaperSize, elements::{Text, TableLayout, LinearLayout, FrameCellDecorator}, Margins, SimplePageDecorator, style::{Style, StyledString}, error::Error, Element};
 
 fn write_text(doc: &mut Document, text: &str, text_style: Style) {
     for text_line in text.split("\n").collect::<Vec<&str>>().iter() {
@@ -14,7 +14,7 @@ fn add_table_row_with_text(table: &mut TableLayout, text_for_columns: Vec<&str>,
         for text_line_for_column in text_for_columns[i].split("\n").collect::<Vec<&str>>().iter() {
             linear_layout.push(Text::new(StyledString::new(*text_line_for_column, style_for_columns[i])));
         }
-        table_row.push_element(linear_layout);
+        table_row.push_element(linear_layout.padded(1));
     }
     table_row.push()
 }
@@ -30,7 +30,7 @@ fn main() {
     write_text(&mut doc, "Max Sendemann\nSendestraße 101\n45678 Sendestadt\n\n\n\n\n\n\n\n", Style::new());
     write_text(&mut doc, "Mustermann Consulting\nMusterstraße 10\n44444 Musterstadt\n", Style::new());
     write_text(&mut doc, "RECHNUNG\n", Style::new().bold());
-    write_text(&mut doc, "Sehr geehrter Kunde,\n\nwie vereinbart berechne ich für meine Leistung wie folgt:", Style::new());
+    write_text(&mut doc, "Sehr geehrter Kunde,\n\nwie vereinbart berechne ich für meine Leistung wie folgt:\n", Style::new());
 
     let mut table = TableLayout::new(vec![4, 1]);
     table.set_cell_decorator(FrameCellDecorator::new(true, true, true));
@@ -40,6 +40,10 @@ fn main() {
     add_table_row_with_text(&mut table, vec!["Telegram Bot (Linux)\nAktueller Stundensatz: 1 Stunden a 50 €"        , "30   €"], vec![Style::new(); 2]).unwrap();
     add_table_row_with_text(&mut table, vec!["PasswordCardCreator (Linux)\nAktueller Stundensatz: 19 Stunden a 50 €", "950  €"], vec![Style::new(); 2]).unwrap();
     add_table_row_with_text(&mut table, vec!["Rechnungsbetrag"                                                      , "1490 €"], vec![Style::new(); 2]).unwrap();
+
+    doc.push(table);
+
+    write_text(&mut doc, "Der Betrag ist bitte bis zum 21.04.2023 auf das, unten stehende, Konto zu überweisen.\n\nHochachtungsvoll\n\n\nMax Sendemann", Style::new());
 
     doc.render_to_file("rechnung.pdf").unwrap();
 }
